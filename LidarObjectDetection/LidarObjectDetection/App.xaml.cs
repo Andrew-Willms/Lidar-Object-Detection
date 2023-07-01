fec6e88b18c5cd2ed73581d1d875dd574f8270bd
Andrew-Willms <andrew.e.willms@gmail.com>
2023-06-30 22:44:32 -0400

    Created LidarObjectDetection solution.

---
+++ LidarObjectDetection/LidarObjectDetection/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace LidarObjectDetection;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ LidarObjectDetection/LidarObjectDetection/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace LidarObjectDetection;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
