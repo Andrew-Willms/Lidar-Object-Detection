80d698e563c563a05c4b50d01f9cb34e5017d1bc
Andrew-Willms <andrew.e.willms@gmail.com>
2023-07-03 14:15:55 -0400

    Got FieldUnitCanvas working.

--- LidarObjectDetection/LidarObjectDetection/MainPage.xaml.cs
+++ LidarObjectDetection/LidarObjectDetection/MainPage.xaml.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.ObjectModel;
-using System.Diagnostics;
 
 namespace LidarObjectDetection;
 
@@ -7,7 +6,7 @@ namespace LidarObjectDetection;
 
 public partial class MainPage : ContentPage {
 
-	public FieldDrawingManager Drawable { get; } = new(1, 3);
+	public FieldDrawingManager Drawable { get; } = new(1, 2);
 
 	public MainPage() {
 
@@ -27,14 +26,14 @@ public partial class MainPage : ContentPage {
 
 public class FieldUnitsCanvas {
 
-	private Color BackgroundColor { get; init; }
-	private Color BorderColor { get; init; }
-	private float BorderThickness { get; init; }
+	private Color BackgroundColor { get; }
+	private Color BorderColor { get; }
+	private float BorderThickness { get; }
 
-	public double FieldWidth { get; }
-	public double FieldHeight { get; }
+	private double FieldWidth { get; }
+	private double FieldHeight { get; }
 	private ICanvas BackingCanvas { get; }
-	private RectF CanvasDimensions { get; }
+	private RectF CanvasDimensionsMinusBorder { get; }
 
 	private readonly double XOffset;
 	private readonly double YOffset;
@@ -46,7 +45,7 @@ public class FieldUnitsCanvas {
 		ICanvas backingCanvas,
 		RectF canvasDimensions,
 		Color backgroundColor,
-		Color borderColor = Colors.White,
+		Color borderColor,
 		float borderThickness = 1) {
 
 		FieldWidth = fieldWidth;
@@ -55,28 +54,44 @@ public class FieldUnitsCanvas {
 		BorderColor = borderColor;
 		BorderThickness = borderThickness;
 		BackingCanvas = backingCanvas;
-		CanvasDimensions = canvasDimensions;
+		CanvasDimensionsMinusBorder = new(0, 0, canvasDimensions.Width - 2 * BorderThickness, canvasDimensions.Height - 2 * BorderThickness);
 
-		double canvasRatio = CanvasDimensions.Width / CanvasDimensions.Height;
+		double canvasRatio = CanvasDimensionsMinusBorder.Width / CanvasDimensionsMinusBorder.Height;
 		double fieldRatio = fieldWidth / fieldHeight;
 
 		bool canvasHasExtraWidth = canvasRatio > fieldRatio;
 
 		if (canvasHasExtraWidth) {
-			DrawScalingFactor = CanvasDimensions.Height / fieldHeight;
-			XOffset = (CanvasDimensions.Width - fieldWidth * DrawScalingFactor) / 2;
-			YOffset = 0;
+			DrawScalingFactor = CanvasDimensionsMinusBorder.Height / fieldHeight;
+			XOffset = (CanvasDimensionsMinusBorder.Width - fieldWidth * DrawScalingFactor) / 2 + BorderThickness;
+			YOffset = BorderThickness;
 
 		} else {
-			DrawScalingFactor = CanvasDimensions.Width / fieldWidth;
-			XOffset = 0;
-			YOffset = (CanvasDimensions.Height - fieldHeight * DrawScalingFactor) / 2;
+			DrawScalingFactor = CanvasDimensionsMinusBorder.Width / fieldWidth;
+			XOffset = BorderThickness;
+			YOffset = (CanvasDimensionsMinusBorder.Height - fieldHeight * DrawScalingFactor) / 2 + BorderThickness;
 		}
 
-		BackingCanvas.DrawRectangle();
+		DrawBorderAndBackground();
 	}
 
 	public void DrawLine(double xStart, double yStart, double xEnd, double yEnd) {
+
+		if (xStart < 0 || xStart > FieldWidth) {
+			throw new ArgumentException($"{nameof(xStart)} must be between {0} and {FieldWidth}, was {xStart}.");
+		}
+
+		if (yStart < 0 || xStart > FieldHeight) {
+			throw new ArgumentException($"{nameof(yStart)} must be between {0} and {FieldHeight}, was {yStart}.");
+		}
+
+		if (xEnd < 0 || xEnd > FieldWidth) {
+			throw new ArgumentException($"{nameof(xEnd)} must be between {0} and {FieldWidth}, was {xEnd}.");
+		}
+
+		if (yEnd < 0 || yEnd > FieldHeight) {
+			throw new ArgumentException($"{nameof(yEnd)} must be between {0} and {FieldHeight}, was {yEnd}.");
+		}
 
 		BackingCanvas.DrawLine(ToCanvasXPosition(xStart), ToCanvasYPosition(yStart), ToCanvasXPosition(xEnd), ToCanvasYPosition(yEnd));
 	}
@@ -89,6 +104,25 @@ public class FieldUnitsCanvas {
 	private float ToCanvasYPosition(double yPosition) {
 
 		return (float)(YOffset + yPosition * DrawScalingFactor);
+	}
+
+	private void DrawBorderAndBackground() {
+
+		float fieldLeftX = ToCanvasXPosition(0);
+		float fieldWidth = ToCanvasXPosition(FieldWidth) - ToCanvasXPosition(0);
+		float fieldTopY = ToCanvasYPosition(0);
+		float fieldHeight = ToCanvasYPosition(FieldHeight) - ToCanvasYPosition(0);
+
+		float borderLeftX = fieldLeftX - BorderThickness;
+		float borderWidth = fieldWidth + 2 * BorderThickness;
+		float borderTopY = fieldTopY - BorderThickness;
+		float borderHeight = fieldHeight + 2 * BorderThickness;
+
+		BackingCanvas.FillColor = BorderColor;
+		BackingCanvas.FillRectangle(borderLeftX, borderTopY, borderWidth, borderHeight);
+
+		BackingCanvas.FillColor = BackgroundColor;
+		BackingCanvas.FillRectangle(fieldLeftX, fieldTopY, fieldWidth, fieldHeight);
 	}
 
 }
@@ -109,51 +143,9 @@ public class FieldDrawingManager : IDrawable {
 		canvas.StrokeColor = Colors.Red;
 		canvas.StrokeSize = 6;
 
-		FieldUnitsCanvas fieldCanvas = new(FieldWidth, FieldHeight, canvas, dirtyRect);
+		FieldUnitsCanvas fieldCanvas = new(FieldWidth, FieldHeight, canvas, dirtyRect, Colors.DarkGray, Colors.White, 5);
 
-		DrawFieldBorders(fieldCanvas);
-	}
-
-	private void DrawFieldBorders(FieldUnitsCanvas fieldCanvas) {
-
-		fieldCanvas.DrawLine(0, 0, FieldWidth, 0);
-		fieldCanvas.DrawLine(FieldWidth, 0, FieldWidth, FieldHeight);
-		fieldCanvas.DrawLine(FieldWidth, FieldHeight, 0, FieldHeight);
-		fieldCanvas.DrawLine(0, FieldHeight, 0, 0);
-	}
-
-}
-
-public class TestDrawable : IDrawable {
-
-	public Setup FieldSetup { get; set; } = new();
-
-	public List<CrossSection> CrossSections { get; } = new();
-
-	public void Draw(ICanvas canvas, RectF rectangle) {
-
-		Debug.WriteLine(canvas);
-		Debug.WriteLine(rectangle);
-
-		canvas.StrokeColor = Colors.Red;
-		canvas.StrokeSize = 6;
-
-		canvas.DrawCircle(new(10, 10), 10.0);
-
-		canvas.DrawLine(10, 10, 90, 100);
-		canvas.DrawRectangle(0, 0, 400, 400);
-	}
-
-	public void DrawFieldBounds(ICanvas canvas, RectF rectangle) {
-
-		double canvasRatio = rectangle.Width / rectangle.Height;
-		double fieldRatio = FieldSetup.FieldWidth / FieldSetup.FieldHeight;
-
-		bool canvasHasExtraWidth = canvasRatio > fieldRatio;
-
-		double drawRatio = canvasHasExtraWidth
-			? rectangle.Height / FieldSetup.FieldHeight
-			: rectangle.Width / FieldSetup.FieldWidth;
+		fieldCanvas.DrawLine(0.25, 0.5, 0.75, 1.5);
 	}
 
 }
