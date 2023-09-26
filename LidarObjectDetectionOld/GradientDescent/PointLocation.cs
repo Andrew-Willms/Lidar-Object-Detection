8f900a9c6e982f5c827654bc833af801a4c77ab0
Andrew-Willms <andrew.e.willms@gmail.com>
2023-09-26 10:58:01 -0400

    Formatting.

--- LidarObjectDetectionOld/GradientDescent/PointLocation.cs
+++ LidarObjectDetectionOld/GradientDescent/PointLocation.cs
@@ -6,8 +6,6 @@ namespace GradientDescent;
 
 public static class PointLocation {
 
-
-
 	public static double DistanceToNearestLidarPoint(IEnumerable<Point2> lidarPoints, Point2 point) {
 
 		return lidarPoints.Select(lidarPoint => new Vector2(lidarPoint, point).Magnitude).Min();
