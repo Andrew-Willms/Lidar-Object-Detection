b119218d54f4b7c5a73107fe64b384ac09f7b326
Andrew-Willms <andrew.e.willms@gmail.com>
2023-09-26 12:46:12 -0400

    Added classes to LinearAlgebra

---
+++ LidarObjectDetection/LinearAlgebra/Polygon.cs
@@ -0,0 +1,7 @@
+﻿namespace LinearAlgebra; 
+
+
+
+public class Polygon {
+	
+}
---
+++ LidarObjectDetection/LinearAlgebra/RectangularRegion.cs
@@ -0,0 +1,11 @@
+﻿namespace LinearAlgebra; 
+
+
+
+public class RectangularRegion {
+
+	public required Point3 CornerA { get; set; }
+
+	public required Point3 CornerB { get; set; }
+
+}
