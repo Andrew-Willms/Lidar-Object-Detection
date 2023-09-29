24975626511351d57174013d71ad51668919b55e
Andrew-Willms <andrew.e.willms@gmail.com>
2023-09-28 23:21:26 -0400

    created InitialGradientApproximations for the first calculation of the gradient whent here is no previous point

---
+++ LidarObjectDetection/LinearAlgebra/GradientDescent/InitialGradientApproximations.cs
@@ -0,0 +1,13 @@
+﻿using LinearAlgebra;
+
+namespace LidarObjectDetection;
+
+
+
+public delegate Vector3 InitialGradientApproximation(Func<Point3, double> function, Point3 point);
+
+
+
+public static class InitialGradientApproximations {
+	
+}
