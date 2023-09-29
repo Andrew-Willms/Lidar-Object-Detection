77540f893c4090e5a6f92aff3e7c023419b4dac8
Andrew-Willms <andrew.e.willms@gmail.com>
2023-09-28 23:16:42 -0400

    Renmaed NearestDistanceCalculator to LeastDistanceCalculator, creaTed ILeastDistanceCalculator, and basic implemenation

---
+++ LidarObjectDetection/LidarObjectDetection/LeastDistanceCalculators.cs
@@ -0,0 +1,38 @@
+﻿using LinearAlgebra;
+
+namespace LidarObjectDetection;
+
+
+
+public delegate ILeastDistanceCalculator LeastDistanceCalculatorCreator(Point2[] otherPoints);
+
+
+
+public interface ILeastDistanceCalculator {
+
+	public double DistanceTo(Point2 point);
+
+}
+
+
+
+public class LeastDistanceCalculator : ILeastDistanceCalculator {
+
+	private Point2[] OtherPoints { get; }
+
+	private LeastDistanceCalculator(Point2[] otherPoints) {
+		OtherPoints = otherPoints;
+	}
+
+	public static LeastDistanceCalculator? Create(Point2[] otherPoints) {
+
+		return otherPoints.Any() 
+			? new(otherPoints) 
+			: null;
+	}
+
+	public double DistanceTo(Point2 point) {
+		return OtherPoints.Select(otherPoint => new Vector2(otherPoint, point).Magnitude).Min();
+	}
+
+}
