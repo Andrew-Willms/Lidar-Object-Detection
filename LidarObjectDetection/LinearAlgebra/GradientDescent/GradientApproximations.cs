4fef2a40f74c037847ef53b0c43b827a65175dea
Andrew-Willms <andrew.e.willms@gmail.com>
2023-09-29 00:03:05 -0400

    Created StepSizeCalculator, added implementation to InitialGradientApproximations, finished signature of GradientApproximation.

--- LidarObjectDetection/LinearAlgebra/GradientDescent/GradientApproximations.cs
+++ LidarObjectDetection/LinearAlgebra/GradientDescent/GradientApproximations.cs
@@ -1,40 +1,26 @@
-﻿using LinearAlgebra;
-
-namespace LidarObjectDetection;
+﻿namespace LinearAlgebra.GradientDescent;
 
 
 
-public delegate Vector3 GradientApproximation(Func<Point3, double> function, Point3 point, Vector3 previousGradient);
-
-public delegate Vector3 GradientStepSizeCalculator(Vector3 gradient);
+public delegate Vector3 GradientApproximation(Func<Point3, double> function, Point3 point, Vector3 previousGradient, Vector3 approximationDeltaSize);
 
 
 
 public static class GradientApproximations {
 
-	public static Vector3 Basic(Func<Point3, double> function, Point3 startingPoint, GradientStepSizeCalculator stepSizeCalculator) {
+	public static Vector3 Basic(Func<Point3, double> function, Point3 point, Vector3 previousGradient, Vector3 approximationDeltaSize) {
 
-		double valueAtPoint = function(startingPoint);
+		double valueAtPoint = function(point);
 
-		double valueAtXOffset = function(startingPoint.Translate(new() { X = DerivativeStepSize, Y = 0, Z = 0 }));
-		double valueAtYOffset = function(startingPoint.Translate(new() { X = 0, Y = DerivativeStepSize, Z = 0 }));
-		double valueAtZOffset = function(startingPoint.Translate(new() { X = 0, Y = 0, Z = DerivativeStepSize }));
+		double valueAtXOffset = function(point.Translate(new() { X = approximationDeltaSize.X, Y = 0, Z = 0 }));
+		double valueAtYOffset = function(point.Translate(new() { X = 0, Y = approximationDeltaSize.Y, Z = 0 }));
+		double valueAtZOffset = function(point.Translate(new() { X = 0, Y = 0, Z = approximationDeltaSize.Z }));
 
-		double xPartial = (valueAtXOffset - valueAtPoint) / DerivativeStepSize;
-		double yPartial = (valueAtYOffset - valueAtPoint) / DerivativeStepSize;
-		double zPartial = (valueAtZOffset - valueAtPoint) / DerivativeStepSize;
+		double xPartial = (valueAtXOffset - valueAtPoint) / approximationDeltaSize.X;
+		double yPartial = (valueAtYOffset - valueAtPoint) / approximationDeltaSize.Y;
+		double zPartial = (valueAtZOffset - valueAtPoint) / approximationDeltaSize.Z;
 
 		return new() { X = xPartial, Y = yPartial, Z = zPartial };
-	}
-
-}
-
-
-
-public static class GradientStepSizeCalculators {
-
-	public static Vector3 LimitedScalingFactor(Vector3 scalingFactor, Vector3 maximumStepSize) {
-
 	}
 
 }
---
+++ LidarObjectDetection/LinearAlgebra/GradientDescent/StepSizeCalculators.cs
@@ -0,0 +1,16 @@
+﻿namespace LinearAlgebra.GradientDescent;
+
+
+
+public delegate Vector3 StepSizeCalculator(Vector3 gradient);
+
+
+
+public static class StepSizeCalculators {
+
+	public static Vector3 LimitedScalingFactor(Vector3 scalingFactor, Vector3 maximumStepSize) {
+
+		throw new NotImplementedException();
+	}
+
+}
