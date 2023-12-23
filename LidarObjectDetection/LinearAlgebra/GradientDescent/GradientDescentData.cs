c518695bc4a1e88821f16d8f6f2627edda631455
Andrew Willms <AndrewW@hyivy.com>
2023-12-23 00:09:16 -0500

    added gradientDescentData

---
+++ LidarObjectDetection/LinearAlgebra/GradientDescent/GradientDescentData.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace LinearAlgebra.GradientDescent; 
+
+
+
+public class GradientDescentData {
+	
+	public required GradientDescentParameters Parameters { get; init; }
+
+	public required List<Point3> Points { get; init; }
+
+	public required List<Vector3> Gradients { get; init; }
+
+	public required List<Vector3> Steps { get; init; }
+
+}
