9d8c735205f6edef56d237c1f9134e6127e8772b
Andrew Willms <AndrewW@hyivy.com>
2023-12-23 22:35:06 -0500

    simplified gradient approximations

--- LidarObjectDetection/LinearAlgebra/GradientDescent/GradientApproximations.cs
+++ LidarObjectDetection/LinearAlgebra/GradientDescent/GradientApproximations.cs
@@ -6,15 +6,13 @@ namespace LinearAlgebra.GradientDescent;
 
 public delegate Vector3 InitialGradientApproximation(Func<Point3, double> function, Point3 point);
 
-
-
 public delegate Vector3 GradientApproximation(Func<Point3, double> function, Point3 point, Vector3 previousStep);
 
 
 
 public static class GradientApproximations {
 
-	public static Vector3 Basic(Func<Point3, double> function, Point3 point, Vector3 approximationDeltaSize) {
+	public static Vector3 InitialConstantDifference(Func<Point3, double> function, Point3 point, Vector3 approximationDeltaSize) {
 
 		double valueAtPoint = function(point);
 
@@ -29,25 +27,9 @@ public static class GradientApproximations {
 		return new() { X = xPartial, Y = yPartial, Z = zPartial };
 	}
 
-}
+	public static Vector3 ConstantDifference(Func<Point3, double> function, Point3 point, Vector3 previousStep, Vector3 approximationDeltaSize) {
 
-
-
-public static class InitialGradientApproximations {
-
-	public static Vector3 Basic(Func<Point3, double> function, Point3 point, Vector3 previousStep, Vector3 approximationDeltaSize) {
-
-		double valueAtPoint = function(point);
-
-		double valueAtXOffset = function(point.Translate(new() { X = approximationDeltaSize.X, Y = 0, Z = 0 }));
-		double valueAtYOffset = function(point.Translate(new() { X = 0, Y = approximationDeltaSize.Y, Z = 0 }));
-		double valueAtZOffset = function(point.Translate(new() { X = 0, Y = 0, Z = approximationDeltaSize.Z }));
-
-		double xPartial = (valueAtXOffset - valueAtPoint) / approximationDeltaSize.X;
-		double yPartial = (valueAtYOffset - valueAtPoint) / approximationDeltaSize.Y;
-		double zPartial = (valueAtZOffset - valueAtPoint) / approximationDeltaSize.Z;
-
-		return new() { X = xPartial, Y = yPartial, Z = zPartial };
+		return InitialConstantDifference(function, point, approximationDeltaSize);
 	}
 
 }
