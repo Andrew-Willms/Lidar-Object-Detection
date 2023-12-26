344a3274b6fd1e17dcdd589c5a064a6a9667adbc
Andrew Willms <AndrewW@hyivy.com>
2023-12-25 23:31:20 -0500

    added linq extensions project

---
+++ LidarObjectDetection/LinqUtilities/LinqExtensions.cs
@@ -0,0 +1,27 @@
+﻿namespace LinqUtilities;
+
+
+
+public static class LinqExtensions {
+
+	public static IEnumerable<(T first, T second)> AdjacentPairs<T>(this IEnumerable<T> enumerable) {
+
+		T[] array = enumerable as T[] ?? enumerable.ToArray();
+
+		for (int index = 0; index < array.Length - 1;) {
+
+			yield return (array[index], array[++index]);
+		}
+	}
+
+	public static IEnumerable<(T first, T second)> AdjacentPairsWrapped<T>(this IEnumerable<T> enumerable) {
+
+		T[] array = enumerable as T[] ?? enumerable.ToArray();
+
+		for (int index = 0; index < array.Length - 1;) {
+
+			yield return (array[index], array[++index % array.Length]);
+		}
+	}
+
+}
