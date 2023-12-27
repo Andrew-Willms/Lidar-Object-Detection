dd97e618053d7b44e3b91db8fc4f502ae1c65355
Andrew Willms <AndrewW@hyivy.com>
2023-12-26 23:42:03 -0500

    added forwards direction back to LidarScanner and turned LidarScanner into a readonly struct

--- LidarObjectDetection/LidarObjectDetection/LidarScanner.cs
+++ LidarObjectDetection/LidarObjectDetection/LidarScanner.cs
@@ -4,9 +4,11 @@ namespace LidarObjectDetection;
 
 
 
-public class LidarScanner {
+public readonly struct LidarScanner {
 
 	public required Point2 Center { get; init; }
+
+	public required Vector2 ForwardsDirection { get; init; }
 
 	public required LineSegment[] Beams { get; init; }
 
